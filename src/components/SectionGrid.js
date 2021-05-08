373096130eae6fb5403488c10c6aa5509f619cad
Stackbit Code Editor <projects@stackbit.com>
2021-05-08 11:18:16 +0000

    SectionGrid.js: changed

--- src/components/SectionGrid.js
+++ src/components/SectionGrid.js
@@ -31,6 +31,20 @@ export default class SectionGrid extends React.Component {
                           <img src={withPrefix(_.get(item, 'image', null))} alt={_.get(item, 'image_alt', null)} />
                         </div>
                         )}
+                        {_.get(item, 'title', null) && (
+                        <h3 className="grid-item-title line-left">
+                          {_.get(item, 'title_url', null) ? (
+                          <Link to={withPrefix(_.get(item, 'title_url', null))}>{_.get(item, 'title', null)}</Link>
+                          ) : 
+                          _.get(item, 'title', null)
+                          }
+                        </h3>
+                        )}
+                        {_.get(item, 'actions', null) && (
+                        <div className="grid-item-buttons">
+                          <CtaButtons {...this.props} actions={_.get(item, 'actions', null)} />
+                        </div>
+                        )}
                       </div>
                     </div>
                     ))}
